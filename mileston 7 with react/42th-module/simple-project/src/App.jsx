b899f48c7b27f960535032b0e5382f3229a9b319
rakib hasan <rakibhasanmd457@gmail.com>
2025-06-24 22:58:02 +0600

    add new

---
+++ mileston 7 with react/42th-module/simple-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='bg-gray-300 font-extrabold text-6xl'>Knowlage cafe</h1>
+      <p className='text-orange-600 text-center'>this name this best real</p>
+    </>
+  )
+}
+
+export default App
